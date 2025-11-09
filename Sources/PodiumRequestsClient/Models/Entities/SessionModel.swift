ec5920ee437f34decb4cfae32577ad3e436c7523
Mathis Le Bonniec <mathislbonniec@gmail.com>
2025-11-09 17:35:46 +0100

    docs(session): add missing session DocC

--- Sources/PodiumRequestsClient/Models/Entities/SessionModel.swift
+++ Sources/PodiumRequestsClient/Models/Entities/SessionModel.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+/// Describe a session, with an ID, location, name, start and end.
+/// A session can be  a race, qualifications, free practice etc.
 @Observable
 public class SessionModel: Identifiable, Equatable {
   // MARK: Properties
@@ -26,6 +28,13 @@ public class SessionModel: Identifiable, Equatable {
   public var end: Date
 
   // MARK: Lifecycle
+  /// Creates a new session.
+  /// - Parameters:
+  ///   - key: The session's unique key.
+  ///   - location: The session's location name.
+  ///   - name: The session's name.
+  ///   - start: The session's start date.
+  ///   - end: The session's end date.
   public init(key: Int, location: String, name: String, start: Date, end: Date) {
     self.key = key
     self.location = location
@@ -35,6 +44,7 @@ public class SessionModel: Identifiable, Equatable {
   }
 
   // MARK: Methods
+  /// Compare two `SessionModel` using their `key` property.
   public static func == (lhs: SessionModel, rhs: SessionModel) -> Bool {
     lhs.key == rhs.key
   }
